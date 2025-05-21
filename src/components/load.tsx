3a85a223a82167fcab78664b33391e2c94769686
Yun4g <delightvincent487@gmail.com>
2025-05-21 13:00:40 +0100

    push new

--- src/components/load.tsx
+++ src/components/load.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const LoadingModal = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900/30">
+    <div className="flex items-center justify-center min-h-screen bg-gray-300/20">
       <div className="flex space-x-2">
         <div className="w-4 h-4 bg-red-600 rounded-full animate-bounce"></div>
         <div className="w-4 h-4 bg-yellow-500 rounded-full animate-bounce delay-200"></div>
